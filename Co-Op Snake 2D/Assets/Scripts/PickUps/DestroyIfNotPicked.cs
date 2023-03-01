92ca93f45393bfe738bf03891a13c29aaca13abb
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-03-01 08:33:38 +0530

    Done

---
+++ Co-Op Snake 2D/Assets/Scripts/PickUps/DestroyIfNotPicked.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class DestroyIfNotPicked : MonoBehaviour
+{
+    [SerializeField]
+    private float waitSeconds = 3f;
+    private void Awake()
+    {
+        StartCoroutine(NotPicked());
+    }
+    IEnumerator NotPicked()
+    {
+        yield return new WaitForSeconds(waitSeconds) ;
+        Destroy(gameObject);
+    }
+}
